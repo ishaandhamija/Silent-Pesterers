334de8f6663a7f1e251ced8a63563d2a4e643402
Ishaan Dhamija <ishaandhamija@Ishaans-MacBook-Air.local>
2017-06-26 11:22:03 +0530

    new interface added

--- app/src/main/java/com/example/ishaandhamija/pesterers/CallStateUtils/CallStateListener.java
+++ app/src/main/java/com/example/ishaandhamija/pesterers/CallStateUtils/CallStateListener.java
@@ -1,7 +1,9 @@
 package com.example.ishaandhamija.pesterers.CallStateUtils;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.media.AudioManager;
+import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -9,6 +11,10 @@ import android.widget.Toast;
 
 import com.example.ishaandhamija.pesterers.Activities.MainActivity;
 import com.example.ishaandhamija.pesterers.DBUtils.ContactDetails;
+import com.example.ishaandhamija.pesterers.DBUtils.DBHelper;
+import com.example.ishaandhamija.pesterers.Interfaces.OnCallReceived;
+
+import java.util.ArrayList;
 
 /**
  * Created by ishaandhamija on 22/06/17.
@@ -19,6 +25,31 @@ public class CallStateListener extends PhoneStateListener {
     public static final String TAG = "UU";
     public static AudioManager audiomanager;
     Context context;
+    DBHelper mydb;
+    ArrayList<String> numbersList;
+
+    OnCallReceived onCallReceived = new OnCallReceived() {
+        @Override
+        public void onSuccess(String incomingNumber) {
+            mydb = new DBHelper(context);
+            numbersList = new ArrayList<>();
+            Cursor res = mydb.getAllData();
+            while (res.moveToNext()) {
+                numbersList.add(res.getString(2));
+            }
+            if (numbersList != null){
+                for (int i=0;i<numbersList.size();i++){
+                    String no = numbersList.get(i);
+                    Log.d(TAG, "onCallStateChanged: no = " + no.trim());
+
+                    if (PhoneNumberUtils.compare(no, incomingNumber)){
+                        audiomanager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                        break;
+                    }
+                }
+            }
+        }
+    };
 
     public CallStateListener(Context context) {
         this.context = context;
@@ -31,21 +62,14 @@ public class CallStateListener extends PhoneStateListener {
             case TelephonyManager.CALL_STATE_RINGING:
                 Log.d(TAG, "onCallStateChanged: " + incomingNumber);
                 Toast.makeText(context, incomingNumber, Toast.LENGTH_SHORT).show();
-                if (incomingNumber != null) {
-                    if (MainActivity.getAllContactsArrayList() != null){
-                        for (int i=0;i<MainActivity.getAllContactsArrayList().size();i++){
-                            String no = MainActivity.getAllContactsArrayList().get(i).getNumber();
-                            Log.d(TAG, "onCallStateChanged: no = " + no.trim());
-                            if (no.equals(incomingNumber)){
-                                audiomanager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-                                break;
-                            }
-                        }
-                    }
-                }
+                while (incomingNumber == null) {}
+                Log.d(TAG, "onCallStateChanged: Yaha aaya kya??incomin");
+                onCallReceived.onSuccess(incomingNumber);
                 break;
         }
     }
+
+
 
     public static AudioManager getAudioManager(){
         return audiomanager;
---
+++ app/src/main/java/com/example/ishaandhamija/pesterers/Interfaces/OnCallReceived.java
@@ -0,0 +1,11 @@
+package com.example.ishaandhamija.pesterers.Interfaces;
+
+/**
+ * Created by ishaandhamija on 25/06/17.
+ */
+
+public interface OnCallReceived {
+
+    public void onSuccess(String no);
+
+}
